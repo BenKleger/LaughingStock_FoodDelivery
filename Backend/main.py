225aff9e36fb5c722b8d288b409acd61e7a7ec59
Ben's Laptop <benjaminkleger03@gmail.com>
2026-03-04 12:36:11 -0800

    Updating main to hopefully comply with pylint

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+"""main docstring"""
 
 app = FastAPI()
 
 @app.get("/health")
 def health():
+    """Used to check the health of the application"""
     return {"status": "ok"}
+
