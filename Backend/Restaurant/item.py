b91f8b88ddae6249799a4520915837dacb94089c
Ben's Laptop <benjaminkleger03@gmail.com>
2026-03-05 14:02:37 -0800

    Took oh so long to figure out but finally working, Testing folder initialized, test cases for order added

--- Backend/Restaurant/item.py
+++ Backend/Restaurant/item.py
@@ -1,16 +1,11 @@
 
 class itemClass:
-    itemID: str
-    itemName: str
-    itemTags: list[str]
-    itemPrice: float
-    itemRating: float
 
     def __init__(self, price: float = 0, ID: str = 0,name:str = "", tags: list[str] = [], rating: float = 0):
         """Initialize items with default values if none have been given."""
-        
-        self.itemPrice = price
-        self.itemID = ID
-        self.itemName = name
-        self.itemTags = tags
-        self.itemRating = rating
+            
+        self.itemPrice: float = price
+        self.itemID: str = ID
+        self.itemName: str = name
+        self.itemTags: list[str] = tags
+        self.itemRating: float = rating
