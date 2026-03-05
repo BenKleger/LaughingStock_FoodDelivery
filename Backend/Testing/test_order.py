f9c1e7b8b8a733ca22191a0c42026489ff82c90b
Ben's Laptop <benjaminkleger03@gmail.com>
2026-03-05 11:45:22 -0800

    just testing to see how this works

--- Backend/Testing/test_order.py
+++ Backend/Testing/test_order.py
@@ -1,9 +1,13 @@
-from Backend.Restaurant.item import itemClass
-from Backend.Order.order import orderClass
+#from Backend.Restaurant.item import itemClass
+#from Backend.Order.order import orderClass
 
-def test_payment_total():
-    item1 = itemClass(10)
-    item2 = itemClass(20)
-    order = orderClass([item1, item2])
-    total = order.get_total
-    assert total == 30
+#def test_payment_total():
+#    item1 = itemClass(10)
+#    item2 = itemClass(20)
+#    order = orderClass([item1, item2])
+#    total = order.get_total
+#    assert total == 30
+
+def test_directory():
+    print(test_directory)
+    assert True
