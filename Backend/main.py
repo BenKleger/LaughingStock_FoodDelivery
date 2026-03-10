f56fff7ef858305ceb4276fcb828960eda28d5b9
insightfulBiscuit <93067476+insightfulBiscuit@users.noreply.github.com>
2026-03-10 15:47:31 -0700

    order dataset and order handling implemented

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from FastAPI_DB.routers.users import router as users_router
+from FastAPI_DB.routers.orders import router as orders_router
+from FastAPI_DB.services.orders_service import reset_order_DB
 from User.login_auth import login
 
 app = FastAPI()
@@ -10,6 +12,10 @@ def health():
 
 def test_null():
     assert True is True
-app.include_router(users_router)
 
-user_id = login()
+app.include_router(users_router)
+app.include_router(orders_router)
+
+print(reset_order_DB())
+print("YES")
+# user_id = login()
