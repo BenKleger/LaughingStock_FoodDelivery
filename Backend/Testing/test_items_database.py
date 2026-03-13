64012ade5fdffbd26a28e74aeb0356bf3569fb64
Ben's Laptop <benjaminkleger03@gmail.com>
2026-03-13 14:45:29 -0700

    Altering docustrings and function names for better clarity in database testing files

--- Backend/Testing/test_items_database.py
+++ Backend/Testing/test_items_database.py
@@ -2,7 +2,12 @@ from ..FastAPI_DB.services.items_service import list_items, reset_items_DB
 from ..FastAPI_DB.schemas.item import Item
 
 
-def test_database_creation():
+def test_item_database_creation():
+    """
+    Tests item database creation and population with initial data.
+    
+    Will alter items.json
+    """
     reset_items_DB()
 
     order_database = list_items()
@@ -10,11 +15,11 @@ def test_database_creation():
     assert len(order_database) == 2083
     assert isinstance(order_database[0], Item)
 
-def test_database_content():
-    """Tests database fetch"""
+def test_item_database_content():
+    """
+    Tests item database content retrieval.
+    """
     order_database = list_items()
 
     assert len(order_database) > 0
     assert isinstance(order_database[0], Item)
-
-# python3 -m Backend.Testing.test_items_database
--- Backend/Testing/test_menus_database.py
+++ Backend/Testing/test_menus_database.py
@@ -2,16 +2,23 @@ from ..FastAPI_DB.services.menus_service import list_menus, reset_menus_DB
 from ..FastAPI_DB.schemas.menu import Menu
 
 
-def test_database_creation():
+def test_menu_database_creation():
+    """
+    Tests menu database creation and population with initial data.
+    
+    Will alter menus.json
+    """
     reset_menus_DB()
 
     menus_database = list_menus()
 
+    assert isinstance(menus_database[0], Menu)
     assert len(menus_database) == 100
     
-def test_database_content():
-    """Tests database fetch"""
+def test_menu_database_content():
+    """
+    Tests database fetch
+    """
     menus_database = list_menus()
 
     assert len(menus_database) > 0
-    assert isinstance(menus_database[0], Menu)
