8a5c51aafa032daa42839b27d706e2c28d754050
insightfulBiscuit <93067476+insightfulBiscuit@users.noreply.github.com>
2026-03-15 21:46:36 -0700

    made adjustments to order cost calculations

--- Backend/Testing/test_order_cost.py
+++ Backend/Testing/test_order_cost.py
@@ -1,11 +1,16 @@
 from ..FastAPI_DB.services.orders_service import get_order_by_order_id
+from ..FastAPI_DB.services.items_service import get_item_by_item_ID
+
 from ..User.user_utils import get_order_cost
 
 def test_order_cost():
     """Tests order cost calculations"""
-    user_order = "1d8e87M"
+    user_order = "fac5b0A"
 
-    user_discount = 0.2
     user_tip = 2.0
+    item_price = get_order_by_order_id(user_order)["item_ids"][0]
 
-    assert get_order_cost(user_order, user_tip, user_discount) == 40.59
+    func_cost = get_order_cost(user_order, user_tip)[0]
+    real_cost = item_price + item_price*0.13 + user_tip + (7 if get_order_by_order_id(user_order)["item_ids"][1]*0.7 < 7 else 2 + get_order_by_order_id(user_order)["item_ids"][1]*0.7)
+
+    assert func_cost == round(real_cost, 2)
