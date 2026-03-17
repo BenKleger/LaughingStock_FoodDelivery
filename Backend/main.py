39890bde2a29a45bae1c76b6adf4cb0e4cd3c70c
atruon00 <172180506+atruon00@users.noreply.github.com>
2026-03-17 03:06:41 -0700

    cant figure it out

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from Backend.FastAPI_DB.routers.users import router as users_router
-from Backend.FastAPI_DB.routers.orders import router as orders_router
-from Backend.FastAPI_DB.routers.items import router as items_router
-from Backend.FastAPI_DB.routers.menus import router as menus_router
-from Backend.User.login_auth import login
-from Backend.FastAPI_DB.services.orders_service import reset_order_DB
+from FastAPI_DB.routers.users import router as users_router
+from FastAPI_DB.routers.orders import router as orders_router
+from FastAPI_DB.routers.items import router as items_router
+from FastAPI_DB.routers.menus import router as menus_router
+from User.login_auth import login
+from FastAPI_DB.services.orders_service import reset_order_DB
 
 app = FastAPI()
 
