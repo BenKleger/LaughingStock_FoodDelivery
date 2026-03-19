5d3457f3b035a33881a00ef3eff2297c6e549125
Tom <tom.huang04@gmail.com>
2026-03-19 01:22:37 -0700

    Updated payment processor to use FastAPI

---
+++ Backend/FastAPI_DB/schemas/payment_processor.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import List
+from Backend.FastAPI_DB.schemas import user as userClass
+from Backend.FastAPI_DB.schemas import order as orderClass
+
+class PaymentProcessor(BaseModel):
+    customer: userClass.User
+    order: orderClass.Order
+    user_address: str
+    payment_number: str
+    payment_pin: str
+    payment_method: str
+    card_holder_name: str
+    postal_code: str
+    email: str
+    email_password: str
+
+class PaymentProcessorCreate(BaseModel):
+    customer: userClass.User
+    order: orderClass.Order
+    user_address: str = ""
+    payment_number: str = ""
+    payment_pin: str = ""
+    payment_method: str = ""
+    card_holder_name: str = ""
+    postal_code: str = ""
+    email: str = ""
+    email_password: str = ""
