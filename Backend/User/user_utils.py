514f060e3b25f2c7cd1c1609f424616c7098c57d
Ben's Laptop <benjaminkleger03@gmail.com>
2026-03-23 15:22:28 -0700

    Adding main branching structure for customers and implementing search

--- Backend/User/user_utils.py
+++ Backend/User/user_utils.py
@@ -1,7 +1,9 @@
 import random
 
-from ..FastAPI_DB.services.orders_service import get_order_by_order_id
-from ..FastAPI_DB.services.items_service import get_item_by_item_ID
+from FastAPI_DB.services.orders_service import get_order_by_order_id
+from FastAPI_DB.services.items_service import get_item_by_item_ID
+from FastAPI_DB.services.search_service import create_search
+from FastAPI_DB.schemas.search import SearchCreate, Search
 
 # System variables
 delivery_vars = {
@@ -44,3 +46,117 @@ def get_order_cost(user_order_id: str, tip: float):
 	cost = price + tax + delivery_cost + tip
 
 	return round(cost, 2), round(auto_gen_distance, 2)
+
+
+def customer_branch(user_id: str):
+	"""
+	Responsible for all customer logic in main branch.
+
+	Allows customer users to:
+		0. Search (by restauarants, items, or item_ids)
+		1. View order status on all of their orders
+		2. Create or Edit (assuming it is in being_created status) an order
+		3. Logout
+	"""
+	while(True): # for repeated actions
+		print("Select Valid Option: \n(0) Search\n(1) View Order Status\n(2) Create or Edit Order\n(3) Log out")
+		while(True): # for ensuring valid actions
+			option = input()
+			if (option == "0" or option == "1" or option == "2" or option == "3"):
+				break
+			print("Invalid Entry. Try Again.")
+		if (option == "0"): 
+			search()
+		elif (option == "1"):
+			view_order_status(user_id) 
+		elif (option == "2"):
+			create_or_edit_order(user_id)
+		elif (option == "3"):
+			break
+
+
+def search():
+	"""
+	Allows user to first select their filtering option,
+	then allows them to enter their search query,
+	prints: 
+		Paginated search query results
+		Allowing user to go forward or back pages, or exit.
+	"""
+	while(True): # for repeated searches
+		print("Select Valid Option: \n(0) Search by price low to high\n(1) Search by price high to low\n(2) Exit search engine")
+		while(True): # for ensuring valid entry of filter
+				option = input()
+				if (option == "0" or option == "1" or option == "2"):
+					break
+				print("Invalid Entry. Try Again.")
+		if option == "0":
+			search_filter = "price_low_to_high"
+		elif option == "1":
+			search_filter = "price_high_to_low"
+		else:
+			break
+		
+		print("Enter your search query")
+		search_query = input()
+		
+		search:Search = create_search(SearchCreate(query=search_query, filter=search_filter))
+		index = 0
+		valid_search = display_page(search, index)
+		while(valid_search): # for repeated changes in page
+			print("\nSelect Valid Option: \n(0) Next Page\n(1) Previous Page\n(2) Exit Search")
+			while(True): # for ensuring valid entry of filter
+				option = input()
+				if (option == "0" or option == "1" or option == "2"):
+					break
+				print("Invalid Entry. Try Again.")
+			if option == "0":
+				index+=1
+				display_page(search,index)
+			elif option == "1":
+				index-=1
+				display_page(search,index)
+			else:
+				break
+
+
+	
+def display_page(search: Search, index: int):
+	"""
+	Displays page number index of the search.
+
+	Returns:
+		True if items are within the search.
+		False if no items with the given query exist.
+	"""
+	if(len(search.search_results) == 0):
+		print("No search results\n")
+		return False #
+	
+	num_pages = len(search.search_results)
+	print("Page " + str(index%num_pages+1) + " of "+ str(num_pages+1))
+	
+	for j in range(len(search.search_results[index%num_pages])):
+		item = get_item_by_item_ID(search.search_results[index%num_pages][j])
+		print(str(item.item_id) + "  \t" + str(item.price))
+	pass
+	return True
+
+def view_order_status(user_id):
+	"""
+	Lists all orders corresponding to user, with their associated statuses.
+	
+	"""
+	pass
+
+def create_or_edit_order(user_id):
+	"""
+	Lists all orders corresponding to user
+	"""
+	pass
+
+def driver_branch():
+	pass
+
+def manager_branch():
+	pass
