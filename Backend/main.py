03fc7d3edd7b8e1ea44bd013d100799e737bf15b
Tom <tom.huang04@gmail.com>
2026-03-25 19:40:26 -0700

    corrected imports in main

--- Backend/main.py
+++ Backend/main.py
@@ -5,7 +5,7 @@ from FastAPI_DB.routers.items import router as items_router
 from FastAPI_DB.routers.menus import router as menus_router
 from User.login_auth import login
 from FastAPI_DB.services.users_service import get_user_by_id
-from User.user_utils import customer_branch, driver_branch, manager_branch
+from User import customer_branch, driver_branch, manager_branch
 
 app = FastAPI()
 
