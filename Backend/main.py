6d2785021d5175e27218cf03e3dd8a689fb78915
insightfulBiscuit <93067476+insightfulBiscuit@users.noreply.github.com>
2026-04-05 22:38:21 -0700

    made dashboard adjustments and dynamic

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from FastAPI_DB.routers.users import router as users_router
 from FastAPI_DB.routers.orders import router as orders_router
 from FastAPI_DB.routers.items import router as items_router
@@ -13,6 +14,17 @@ from User.driver_branch import driver_branch
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health():
