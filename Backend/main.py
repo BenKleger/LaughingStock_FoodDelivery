0db18791ecbe23088d59198139486f38d00c16fd
atruon00 <172180506+atruon00@users.noreply.github.com>
2026-04-05 20:04:15 -0700

    endpoints for manager functionalities added

--- Backend/main.py
+++ Backend/main.py
@@ -3,6 +3,7 @@ from FastAPI_DB.routers.users import router as users_router
 from FastAPI_DB.routers.orders import router as orders_router
 from FastAPI_DB.routers.items import router as items_router
 from FastAPI_DB.routers.menus import router as menus_router
+from FastAPI_DB.routers.manager_func import router as manager_router
 from FastAPI_DB.routers.payment import router as payment_router
 from User.login_auth import login
 from FastAPI_DB.services.users_service import get_user_by_id
@@ -23,6 +24,7 @@ app.include_router(orders_router)
 app.include_router(items_router)
 app.include_router(menus_router)
 app.include_router(payment_router)
+app.include_router(manager_router)
 
 if __name__ == "__main__":  
     """Main branch of operations"""
